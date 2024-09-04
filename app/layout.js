2d25ee2e35e34d2a4ca9b0bcb794df6eafcf86bc
Mithlesh Sharma <itsyour.rohan@gmail.com>
2024-09-04 21:20:20 +0530

    favicon added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from 'next/head';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,10 +10,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-       <Head>
-        <link rel="icon" href="/pokeball.png" />
-        <title>Your Website Title</title>
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
